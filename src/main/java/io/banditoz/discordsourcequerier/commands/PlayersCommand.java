9a5638f1e59a0e5ce4418da231903d6628d925fd
HeyBanditoz <banditoz@protonmail.com>
2019-05-09 22:02:26 -0600

    Clarify we cannot get player info if server >100 players.
    See related issue: https://github.com/koraktor/steam-condenser/issues/319

--- src/main/java/io/banditoz/discordsourcequerier/commands/PlayersCommand.java
+++ src/main/java/io/banditoz/discordsourcequerier/commands/PlayersCommand.java
@@ -4,6 +4,7 @@ import io.banditoz.discordsourcequerier.querier.SourceQuerier;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.exceptions.PermissionException;
 
+import java.nio.BufferUnderflowException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -17,10 +18,17 @@ public class PlayersCommand extends Command {
     public void onCommand(MessageReceivedEvent e, String[] commandArgs) {
         try {
             SourceQuerier server = new SourceQuerier(commandArgs[1]);
-            server.getPlayers().setFinalAction(m -> {try{m.clearReactions().queue();}catch (PermissionException ex){sendExceptionMessage(e, ex);}})
+            server.getPlayers().setFinalAction(m -> {
+                try {
+                    m.clearReactions().queue();
+                } catch (PermissionException ex) {
+                    sendExceptionMessage(e, ex);
+                }
+            })
                     .build().paginate(e.getChannel(), 1);
-        }
-        catch (Exception ex) {
+        } catch (BufferUnderflowException ex) {
+            sendReply(e, "Unfortunately, due to the way Garry's Mod sends packets containing player information, this bot cannot retrieve the player list if the server has over 100 players.");
+        } catch (Exception ex) {
             sendExceptionMessage(e, ex);
         }
     }
